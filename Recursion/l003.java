0b2cd268c42ced5f692a2e350aaf9cd21fd9bc02
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2024-12-06 13:10:20 +0530

    Updated code

--- Recursion/l003.java
+++ Recursion/l003.java
@@ -2,8 +2,12 @@ public class l003 {
     public static void main(String[] args){
        String str = "aabcdeaaf";
        char ch = 'a';
-       moveCharactertoEnd(str, "", 0, 0, ch);
+      // moveCharactertoEnd(str, "", 0, 0, ch);
+      // removeAllHi("hhiij", "", 0);
+
+      getPath(0, 0, 2, 2, "");
     }
+    
 
     public static void moveCharactertoEnd(String str, String ans, int idx, int count, char ch) {
 
@@ -25,4 +29,54 @@ public class l003 {
         }
     
       }
+    
+
+    public static void removeAllHi(String str, String ans, int idx){
+         
+        if(idx >= str.length() - 1){
+        if(idx == str.length() - 1){
+            ans = ans + str.charAt(str.length() - 1);
+        }
+        System.out.println(ans);
+        return;
+       
+    }  
+    char prev = ans.length() > 0? (ans.charAt(ans.length()-1)) :  (' ');
+    
+        char ch = str.charAt(idx);
+        char ch1 = str.charAt(idx + 1);
+
+        if(ch == 'i' && prev == 'h'){
+        ans = ans.substring(0,ans.length()-1);
+        removeAllHi(str, ans, idx + 1);
     }
+       else if(ch == 'h' && ch1 == 'i'){
+           removeAllHi(str, ans, idx + 2);
+        }else{
+           removeAllHi(str, ans + ch, idx + 1);
+        }
+    }
+
+
+    public static void getPath(int sc, int sr, int dc, int dr, String ans){
+        
+        if(sr == dr && sc == dc){
+            System.out.println(ans);
+            return;
+        }
+
+        if(sc + 1 <= dr){
+        getPath(sc + 1, sr, dc, dr, ans + "H");
+        }
+        if(sr + 1 <= dr){
+        getPath(sc, sr + 1, dc, dr, ans + "V");
+        }
+        if(sr + 1 <= dc && sc + 1 <= dc){
+        getPath(sc + 1, sr + 1, dc, dr, ans + "D");
+        }
+
+
+
+    }
+
+}
