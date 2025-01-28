578e968968e5b39174b9d6dce686fa482da2e2ee
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-01-28 15:53:21 +0530

    Update

--- Graph/ayush.java
+++ Graph/ayush.java
@@ -20,6 +20,7 @@ public class ayush {
     for(int i = 0; i < N; i++){
         graph[i] = new ArrayList<>();
     }
+    boolean[] vis = new boolean[N];
         addEdge(0, 1, 10);
         addEdge(0, 3, 40);
         addEdge(1, 2, 10);
@@ -29,10 +30,14 @@ public class ayush {
         addEdge(4, 6, 8);
         addEdge(5, 6, 3);
     
-        display();
+        //display();
         //System.out.println(findEdge(0, 1));
         //removeEdge(2, 3);
         //display();
+       //removeVtx(3);
+       // System.out.println(hasPath(0, 6, vis));
+        allPath(0, 6, vis, new StringBuilder());
+        
     }
    
    
@@ -79,8 +84,59 @@ public class ayush {
    }
 
    public static void removeVtx(int u){
-
+        ArrayList<Edge> list = graph[u];
+        for(int i = list.size() - 1; i >= 0; i--){
+            Edge e = list.get(i);
+            removeEdge(u, e.v);
+        }
    }
 
+   public static boolean hasPath(int src, int des, boolean[]vis){
+    if(src == des)return true;
+
+    vis[src] = true;
+    for(Edge e : graph[src]){
+        if(!vis[e.v]){
+          boolean res = hasPath(e.v, des, vis);
+          if(res)return res;
+        }
+    }
+    return false;
+   }
+
+   public static void allPath(int src, int des, boolean[]vis, StringBuilder ans){
+    if(src == des){
+        System.out.println(ans.toString() + src);
+        return;
+    }
+
+    vis[src] = true;
+    ans.append(src); // 012345
+    for(Edge e : graph[src]){ //13 02 13 024 356 
+        if(!vis[e.v]){ //
+         allPath(e.v, des, vis, ans);//1 | 2 | 3 | 4 | 5,6
+        }
+    }
+    ans.deleteCharAt(ans.length() - 1); //5
+    vis[src] = false;
+    return;
+   }
    
+   public static void dfs(int src, boolean[] vis) {
+    vis[src] = true;
+    for (Edge e : graph[src])
+        if (!vis[e.v])
+            dfs(e.v, vis);
+}
+
+   public static void gcc() {
+    boolean[] vis = new boolean[N];
+    int components = 0;
+    for (int i = 0; i < N; i++) {
+        if (!vis[i]) {
+            components++;
+            dfs(i, vis);
+        }
+    }
+}
 }
