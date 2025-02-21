937af586b42bee27dee611b3573b318d0ca03c49
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-02-21 20:17:41 +0530

    Update

--- Graph/dsuQuestions.java
+++ Graph/dsuQuestions.java
@@ -192,4 +192,36 @@ public class dsuQuestions {
 
         return ans;
     }
+
+
+    // 1168
+    public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
+        ArrayList<int[]> allPipes = new ArrayList<>();
+        for (int[] a : pipes)
+            allPipes.add(a);
+        for (int i = 0; i < wells.length; i++)
+            allPipes.add(new int[] { 0, i + 1, wells[i] });
+
+        Collections.sort(allPipes, (a, b) -> {
+            return a[2] - b[2];
+        });
+
+        par = new int[n + 1];
+        int ans = 0;
+
+        for (int i = 0; i <= n; i++) {
+            par[i] = i;
+        }
+
+        for (int[] e : allPipes) {
+            int u = e[0], v = e[1], w = e[2];
+            int p1 = findPar(u), p2 = findPar(v);
+            if (p1 != p2) {
+                par[p1] = p2;
+                ans += w;
+            }
+        }
+
+        return ans;
+    }
 }
