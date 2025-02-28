cc5dc24a4d6ed67d39baac71f747c48959333af0
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-02-28 13:36:04 +0530

    Update

--- Array_Strings/l001.java
+++ Array_Strings/l001.java
@@ -1,5 +1,16 @@
-package Array_Strings;
-
 public class l001 {
     
+    public static int max(int[] arr){
+        int maxResult = -(int)1e9;
+
+        for(int i = 0; i < arr.length; i++){
+            maxResult = Math.max(maxResult, arr[i]);
+        }
+
+        return maxResult;
+    }
+    public static void main(String[] args){
+        int[] arr = {3,2,65,71,99,1};
+        System.out.println(max(arr));
+    }
 }
