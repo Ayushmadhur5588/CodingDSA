7df578f4f53304c36d8a440984d6b8bf6eceec9c
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-03 21:44:01 +0530

    Update

--- Array_Strings/l003.java
+++ Array_Strings/l003.java
@@ -51,4 +51,27 @@ public class l003 {
         return sb.toString();
     }
 
+
+    public static String compression2(String str) {
+        if (str.length() <= 1)
+            return str;
+        StringBuilder sb = new StringBuilder();
+        sb.append(str.charAt(0));
+
+        for (int i = 1; i <= str.length(); i++) {
+            int count = 1;
+            while (i < str.length() && str.charAt(i - 1) == str.charAt(i)) {
+                count++;
+                i++;
+            }
+
+            if (count != 1)
+                sb.append(count);
+            if (i < str.length())
+                sb.append(str.charAt(i));
+        }
+
+        return sb.toString();
+    }
+
 }
