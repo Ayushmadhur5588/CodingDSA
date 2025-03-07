92cca8449a4c0d33d3fbd78108b2679aaeb30073
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-07 22:06:35 +0530

    Update

--- Hashmap/l001.java
+++ Hashmap/l001.java
@@ -97,4 +97,21 @@ public class l001 {
         }
     }
 
+    public static void intersectionOfTwoArrayWithDuplicates(int[] arr1, int[] arr2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int ele : arr1) {
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        }
+
+        for (int ele : arr2) {
+            if (map.containsKey(ele)) {
+                System.out.println(ele);
+
+                map.put(ele, map.get(ele) - 1);
+                if (map.get(ele) == 0)
+                    map.remove(ele);
+            }
+        }
+    }
+
 }
