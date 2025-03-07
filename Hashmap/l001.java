4efb385ac6794e6594f66026e9bb34c5ef66d61b
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-07 22:05:13 +0530

    Update

--- Hashmap/l001.java
+++ Hashmap/l001.java
@@ -42,4 +42,22 @@ public class l001 {
         }
     }
 
+    public static void highestFreqChar(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        char ans = ' ';
+        int freq = 0;
+        for (Character ch : map.keySet()) {
+            if (map.get(ch) > freq) {
+                freq = map.get(ch);
+                ans = ch;
+            }
+        }
+
+        System.out.println(ans);
+    }
 }
