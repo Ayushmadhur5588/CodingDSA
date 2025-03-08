7fa55855d2a5c5c9e4435471b664d0598d4bd516
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-08 23:34:11 +0530

    Update

--- Heaps/l001.java
+++ Heaps/l001.java
@@ -117,6 +117,34 @@ public class l001 {
 
         return ans;
     }
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String word : words)
+            map.put(word, map.getOrDefault(word, 0) + 1);
+
+        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> {
+            if (map.get(a) == map.get(b)) {
+                return b.compareTo(a);
+            }
+            return map.get(a) - map.get(b);
+        });
+
+        for (String word : map.keySet()) {
+            pq.add(word);
+            if (pq.size() > k)
+                pq.remove();
+        }
+
+        int idx = pq.size();
+        List<String> ans = new ArrayList<>();
+        for (int i = 0; i < idx; i++)
+            ans.add("");
+        while (pq.size() != 0) {
+            ans.set(--idx, pq.remove());
+        }
+
+        return ans;
+    }
 
 
 }
