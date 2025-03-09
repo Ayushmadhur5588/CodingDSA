388a39e652bf4775fe425cbcb8db3ce03932a2f9
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-09 22:03:45 +0530

    UPdate

---
+++ DP/l001.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Arrays;
+
+public class l001 {
+    public static void print(int[] arr) {
+        for (int ele : arr)
+            System.out.print(ele + " ");
+        System.out.println();
+    }
+
+    public static void print2D(int[][] arr) {
+        for (int[] a : arr) {
+            print(a);
+        }
+
+        System.out.println();
+    }
+
+}
