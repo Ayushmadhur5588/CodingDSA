da2a146678e5cf3ea5c55a991f7fec6fca448b06
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-09 22:04:57 +0530

    Update

--- DP/l001.java
+++ DP/l001.java
@@ -43,4 +43,15 @@ public class l001 {
     }
 
 
+    public static int fibo_Opti(int n) {
+        int a = 0, b = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+
+        return a;
+    }
 }
