8f387ffba0e2fb980092d13309e865287f4156dd
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-12 20:50:13 +0530

    UPdate

--- DP/l001.java
+++ DP/l001.java
@@ -235,4 +235,30 @@ public class l001 {
 
         return dp[sr][sc] = count;
     }
+
+    public static int mazePath_HDV_DP(int SR, int SC, int er, int ec, int[][] dir, int[][] dp) {
+
+        for (int sr = er; sr >= 0; sr--) {
+            for (int sc = ec; sc >= 0; sc--) {
+                if (sr == er && sc == ec) {
+                    dp[sr][sc] = 1;
+                    continue;
+                }
+
+                int count = 0;
+                for (int d = 0; d < dir.length; d++) {
+                    int r = sr + dir[d][0];
+                    int c = sc + dir[d][1];
+                    if (r >= 0 && c >= 0 && r <= er && c <= ec) {
+                        count += dp[r][c];// mazePath_HDV(r, c, er, ec, dir, dp);
+                    }
+                }
+
+                dp[sr][sc] = count;
+            }
+        }
+
+        return dp[SR][SC];
+    }
+
 }
