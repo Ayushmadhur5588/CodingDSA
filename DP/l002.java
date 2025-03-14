7f2ecd18414ca456919a1051e67693a4afdb12b3
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-14 16:57:25 +0530

    Update

--- DP/l002.java
+++ DP/l002.java
@@ -15,4 +15,20 @@ public class l002 {
 
         System.out.println();
     }
+
+    // 516
+    // LPSS : longestPalindromicSubsequence
+    public static int LPSS(String str, int i, int j, int[][] dp) {
+        if (i >= j) {
+            return dp[i][j] = (i == j) ? 1 : 0;
+        }
+
+        if (dp[i][j] != 0)
+            return dp[i][j];
+
+        if (str.charAt(i) == str.charAt(j))
+            return dp[i][j] = LPSS(str, i + 1, j - 1, dp) + 2;
+        else
+            return dp[i][j] = Math.max(LPSS(str, i + 1, j, dp), LPSS(str, i, j - 1, dp));
+    }
 }
