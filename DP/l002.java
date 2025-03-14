8447b0fc090a9286e2823ca6af6070de72a5c8a9
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-14 16:57:48 +0530

    Update

--- DP/l002.java
+++ DP/l002.java
@@ -31,4 +31,23 @@ public class l002 {
         else
             return dp[i][j] = Math.max(LPSS(str, i + 1, j, dp), LPSS(str, i, j - 1, dp));
     }
+
+    public static int LPSS_DP(String str, int I, int J, int[][] dp) {
+        int n = str.length();
+        for (int gap = 0; gap < n; gap++) {
+            for (int i = 0, j = gap; j < n; i++, j++) {
+                if (i >= j) {
+                    dp[i][j] = (i == j) ? 1 : 0;
+                    continue;
+                }
+
+                if (str.charAt(i) == str.charAt(j))
+                    dp[i][j] = dp[i + 1][j - 1] + 2;// LPSS(str, i + 1, j - 1, dp) + 2;
+                else
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[I][J];
+    }
 }
