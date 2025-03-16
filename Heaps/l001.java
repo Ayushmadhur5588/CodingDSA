7b0c283f205819288ce470e7c89831a633d04549
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-16 20:43:48 +0530

    Update

--- Heaps/l001.java
+++ Heaps/l001.java
@@ -164,5 +164,32 @@ public class l001 {
             System.out.println(ele);
     }
 
+    public static ArrayList<Integer> mergeKSortedLists(ArrayList<ArrayList<Integer>> lists) {
+        ArrayList<Integer> rv = new ArrayList<>();
+        // {ele, index, index of list}
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            return a[0] - b[0];
+        });
+
+        for (int i = 0; i < lists.size(); i++)
+            pq.add(new int[] { lists.get(i).get(0), 0, i });
+
+        while (pq.size() != 0) {
+            int[] ar = pq.remove();
+            rv.add(ar[0]);
+
+            int idx = ar[1];
+            int listIdx = ar[2];
+            int length = lists.get(listIdx).size();
+
+            if (idx + 1 < length) {
+                ar[1]++;
+                ar[0] = lists.get(listIdx).get(idx + 1);
+                pq.add(ar);
+            }
+        }
+
+        return rv;
+    }
 
 }
