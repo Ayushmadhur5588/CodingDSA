1437e8193699f76c2d0816fce48a77e9ac50defc
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-17 21:51:03 +0530

    UPdate

--- LinkedList/l001.java
+++ LinkedList/l001.java
@@ -96,6 +96,21 @@ public class l001 {
         reversePRHelper(head);
         tail.next = null;
     }
+    public int kthFromLast(int k) {
+        Node slow = head;
+        Node fast = head;
+
+        while (k-- > 0) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow.data;
+    }
 
 
 }
