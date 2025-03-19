83529ed9678a203f22f5e7f780a0d1c150bc3d62
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-19 21:14:10 +0530

    Update

--- LinkedList/l001.java
+++ LinkedList/l001.java
@@ -151,5 +151,33 @@ public class l001 {
 
         return slow;
     }
+    public static LinkedList addTwoLists(LinkedList one, LinkedList two) {
+        one.reversePI();
+        two.reversePI();
 
+        LinkedList ans = new LinkedList();
+
+        Node c1 = one.head;
+        Node c2 = two.head;
+        int carry = 0;
+        while (c1 != null || c2 != null || carry != 0) {
+            int sum = (c1 != null ? c1.data : 0) + (c2 != null ? c2.data : 0) + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+
+            ans.addFirst(sum);
+
+            if (c1 != null)
+                c1 = c1.next;
+            if (c2 != null)
+                c2 = c2.next;
+
+        }
+
+        one.reversePI();
+        two.reversePI();
+
+        return ans;
+    }
 }
