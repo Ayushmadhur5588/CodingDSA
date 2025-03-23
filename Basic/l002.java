431649f542a37e2610a8b4cdacb06b39c9bd9479
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-23 20:41:39 +0530

    UPdate

--- Basic/l002.java
+++ Basic/l002.java
@@ -158,6 +158,27 @@ public class l002{
 
         return res;
     }
+
+    public static long anyBaseSub(long n,long m,long b){
+        // m > n
+        long borrow = 0, pow = 1, res = 0;
+        while(n != 0 || m != 0){
+            long sum = (m % 10 + borrow) - n % 10;
+            n /= 10;
+            m /= 10;
+
+            if(sum < 0) {
+                sum += b;
+                borrow = -1;
+            }else
+               borrow = 0;
+
+            res += sum * pow;
+            pow *= 10;
+        }
+
+        return res;
+    }
     
 
 }
