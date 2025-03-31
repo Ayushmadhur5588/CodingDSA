a8a8fe32c71fcb57b829c0c126898305f07f2ed5
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-31 20:49:02 +0530

    Update

--- Leetcode/l001.java
+++ Leetcode/l001.java
@@ -91,5 +91,12 @@ public class l001 {
         
             return mx - mn;
           }
+
+
+          public int minimumSum(int num) {
+            int []a={ num%10,(num/10)%10,(num/100)%10 , (num/1000)%10};
+            Arrays.sort(a);
+            return a[0]*10+a[3]+a[1]*10+a[2];
+        }
     }
 
