af5178bda8286dc055ebcf433ae7d09a48bf3fe1
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-04-01 21:40:55 +0530

    Update

--- Leetcode/l001.java
+++ Leetcode/l001.java
@@ -1,144 +1,154 @@
-package Leetcode;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.*;
 
 public class l001 {
-    
-        public int maxProduct(int[] nums) {
-            int ans = -(int) 1e9;
-            int cPro = 1;
-    
-            for (int i = 0; i < nums.length; i++) {
-                cPro *= nums[i];
-                ans = Math.max(ans, cPro);
-                if (cPro == 0) {
-                    cPro = 1;
-                }
+
+    public int maxProduct(int[] nums) {
+        int ans = -(int) 1e9;
+        int cPro = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            cPro *= nums[i];
+            ans = Math.max(ans, cPro);
+            if (cPro == 0) {
+                cPro = 1;
             }
-            cPro = 1;
-            for (int i = nums.length - 1; i >= 0; i--) {
-                cPro *= nums[i];
-                ans = Math.max(ans, cPro);
-                if (cPro == 0) {
-                    cPro = 1;
-                }
+        }
+        cPro = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            cPro *= nums[i];
+            ans = Math.max(ans, cPro);
+            if (cPro == 0) {
+                cPro = 1;
             }
-    
-            return ans;
         }
 
-        public List<Integer> partitionLabels(String s) {
-            Map<Character,Integer> map = new HashMap<>();
-            for(int i=0;i<s.length();i++)
-                map.put(s.charAt(i),i);
-            List<Integer> res = new ArrayList<>();
-            int prev = -1,max = 0;
-            for(int i=0;i<s.length();i++)
-            {
-                max = Math.max(max,map.get(s.charAt(i)));
-                if(max == i)
-                {
-                    res.add(max - prev);
-                    prev = max;
-                }
-            }
-            return res;
-        }
-
-        public int minimumPushes(String word) {
-            int[] ans=new int[26];
-            
-            for(int i=0;i<word.length();i++){
-                int ch=(int)(word.charAt(i)-'a');
-                ans[ch]++;
-            }
-            Arrays.sort(ans);
-            int count=1;
-            int ans1=0;
-            for(int i=25;i>=0;i--){
-                if(ans[i]>0 && count <=8){
-                    ans1=ans1+(1*ans[i]);
-                    count++;
-                }
-                else if(ans[i]>0 && (count>8 && count<=16)){
-                    ans1=ans1+(2*ans[i]);
-                    count++;
-                }
-                else if(ans[i]>0 && (count>16 && count<=24)){
-                    ans1=ans1+(3*ans[i]);
-                    count++;
-                }
-                else if(ans[i]>0 && (count>24)){
-                    ans1=ans1+(4*ans[i]);
-                    count++;
-                }
-            } 
-            return ans1;
-        }
-        public long putMarbles(int[] weights, int k) {
-    
-            int[] arr = new int[weights.length - 1]; 
-            long mn = 0;
-            long mx = 0;
-        
-            for (int i = 0; i < arr.length; ++i)
-              arr[i] = weights[i] + weights[i + 1];
-        
-            Arrays.sort(arr);
-        
-            for (int i = 0; i < k - 1; ++i) {
-              mn += arr[i];
-              mx += arr[arr.length - 1 - i];
-            }
-        
-            return mx - mn;
-          }
-
-          public static int LPSS_DP(String str, int I, int J, int[][] dp) {
-            int n = str.length();
-            for (int gap = 0; gap < n; gap++) {
-                for (int i = 0, j = gap; j < n; i++, j++) {
-                    if (i >= j) {
-                        dp[i][j] = (i == j) ? 1 : 0;
-                        continue;
-                    }
-    
-                    if (str.charAt(i) == str.charAt(j))
-                        dp[i][j] = dp[i + 1][j - 1] + 2;// LPSS(str, i + 1, j - 1, dp) + 2;
-                    else
-                        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
-                }
-            }
-    
-            return dp[I][J];
-        }
-
-          public static String LPSS_StringDP(String str) {
-            int n = str.length();
-            String[][] dp = new String[n][n];
-            for (String[] d : dp)
-                Arrays.fill(d, "");
-    
-            for (int gap = 0; gap < n; gap++) {
-                for (int i = 0, j = gap; j < n; i++, j++) {
-                    if (i >= j) {
-                        dp[i][j] = (i == j) ? str.charAt(i) + "" : "";
-                        continue;
-                    }
-    
-                    if (str.charAt(i) == str.charAt(j))
-                        dp[i][j] = str.charAt(i) + dp[i + 1][j - 1] + str.charAt(j);
-                    else
-                        dp[i][j] = dp[i + 1][j].length() > dp[i][j - 1].length() ? dp[i + 1][j] : dp[i][j - 1];
-                }
-            }
-    
-            return dp[0][n - 1];
-        }
-
-
-          public int minimumSum(int num) {
-            int []a={ num%10,(num/10)%10,(num/100)%10 , (num/1000)%10};
-            Arrays.sort(a);
-            return a[0]*10+a[3]+a[1]*10+a[2];
-        }
+        return ans;
     }
 
+    public List<Integer> partitionLabels(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++)
+            map.put(s.charAt(i), i);
+        List<Integer> res = new ArrayList<>();
+        int prev = -1, max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            max = Math.max(max, map.get(s.charAt(i)));
+            if (max == i) {
+                res.add(max - prev);
+                prev = max;
+            }
+        }
+        return res;
+    }
+
+    public int minimumPushes(String word) {
+        int[] ans = new int[26];
+
+        for (int i = 0; i < word.length(); i++) {
+            int ch = (int) (word.charAt(i) - 'a');
+            ans[ch]++;
+        }
+        Arrays.sort(ans);
+        int count = 1;
+        int ans1 = 0;
+        for (int i = 25; i >= 0; i--) {
+            if (ans[i] > 0 && count <= 8) {
+                ans1 = ans1 + (1 * ans[i]);
+                count++;
+            } else if (ans[i] > 0 && (count > 8 && count <= 16)) {
+                ans1 = ans1 + (2 * ans[i]);
+                count++;
+            } else if (ans[i] > 0 && (count > 16 && count <= 24)) {
+                ans1 = ans1 + (3 * ans[i]);
+                count++;
+            } else if (ans[i] > 0 && (count > 24)) {
+                ans1 = ans1 + (4 * ans[i]);
+                count++;
+            }
+        }
+        return ans1;
+    }
+
+    public long putMarbles(int[] weights, int k) {
+
+        int[] arr = new int[weights.length - 1];
+        long mn = 0;
+        long mx = 0;
+
+        for (int i = 0; i < arr.length; ++i)
+            arr[i] = weights[i] + weights[i + 1];
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < k - 1; ++i) {
+            mn += arr[i];
+            mx += arr[arr.length - 1 - i];
+        }
+
+        return mx - mn;
+    }
+
+    public static int LPSS(String str, int i, int j, int[][] dp) {
+        if (i >= j) {
+            return dp[i][j] = (i == j) ? 1 : 0;
+        }
+
+        if (dp[i][j] != 0)
+            return dp[i][j];
+
+        if (str.charAt(i) == str.charAt(j))
+            return dp[i][j] = LPSS(str, i + 1, j - 1, dp) + 2;
+        else
+            return dp[i][j] = Math.max(LPSS(str, i + 1, j, dp), LPSS(str, i, j - 1, dp));
+    }
+
+    public static int LPSS_DP(String str, int I, int J, int[][] dp) {
+        int n = str.length();
+        for (int gap = 0; gap < n; gap++) {
+            for (int i = 0, j = gap; j < n; i++, j++) {
+                if (i >= j) {
+                    dp[i][j] = (i == j) ? 1 : 0;
+                    continue;
+                }
+
+                if (str.charAt(i) == str.charAt(j))
+                    dp[i][j] = dp[i + 1][j - 1] + 2;// LPSS(str, i + 1, j - 1, dp) + 2;
+                else
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[I][J];
+    }
+
+    public static String LPSS_StringDP(String str) {
+        int n = str.length();
+        String[][] dp = new String[n][n];
+        for (String[] d : dp)
+            Arrays.fill(d, "");
+
+        for (int gap = 0; gap < n; gap++) {
+            for (int i = 0, j = gap; j < n; i++, j++) {
+                if (i >= j) {
+                    dp[i][j] = (i == j) ? str.charAt(i) + "" : "";
+                    continue;
+                }
+
+                if (str.charAt(i) == str.charAt(j))
+                    dp[i][j] = str.charAt(i) + dp[i + 1][j - 1] + str.charAt(j);
+                else
+                    dp[i][j] = dp[i + 1][j].length() > dp[i][j - 1].length() ? dp[i + 1][j] : dp[i][j - 1];
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+
+    public int minimumSum(int num) {
+        int[] a = { num % 10, (num / 10) % 10, (num / 100) % 10, (num / 1000) % 10 };
+        Arrays.sort(a);
+        return a[0] * 10 + a[3] + a[1] * 10 + a[2];
+    }
+}
