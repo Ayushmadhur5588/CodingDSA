c9dc6e82ea40ab846ec647045886fc10bd565097
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-04-06 22:54:04 +0530

    Update

--- Leetcode/l001.java
+++ Leetcode/l001.java
@@ -151,4 +151,16 @@ public class l001 {
         Arrays.sort(a);
         return a[0] * 10 + a[3] + a[1] * 10 + a[2];
     }
+
+    public int tribonacci(int n) {
+        if(n<3){
+           return n < 1 ? 0 : 1;
+        }
+        int dp[]=new int[n+1];
+        dp[0]=0;dp[1]=1;dp[2]=1;
+        for(int i=3;i<=n;i++){
+           dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }   
+        return dp[n];
+       }
 }
